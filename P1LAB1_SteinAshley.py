e5a2043ef91ef9ac86a766c5bd462fcdd46d7648
steina4049 <steina4049@student.faytechcc.edu>
2025-02-24 19:07:02 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_SteinAshley.py
@@ -0,0 +1,10 @@
+# Ashley Stein
+# 2/24/2025
+# P1LAB1
+# Enter full name and display a welcome message.
+first_name = input("Ashley: ")
+last_name = input("Stein: ")
+print()
+print("Hello", first_name, last_name + "!", "Welcome to CTI-110.")
+print()
+print(type(first_name))
