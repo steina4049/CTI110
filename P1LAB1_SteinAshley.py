6b2e7e463d11ac95a90043ab13b24b51039b258f
steina4049 <steina4049@student.faytechcc.edu>
2025-02-28 18:18:51 -0500

    Add files via upload

---
+++ P1LAB1_SteinAshley.py
@@ -0,0 +1,8 @@
+# Ashley Stein
+# 2/28/2025
+# P1LAB1
+# Enter full name and display a welcome message.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print()
+print("Hello", first_name, last_name + "!", "Welcome to CTI-110.")
