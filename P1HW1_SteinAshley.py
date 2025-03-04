bfe2efb71f98979f40af9330c7c2340766bb2ef3
steina4049 <steina4049@student.faytechcc.edu>
2025-03-04 17:24:37 -0500

    Add files via upload

---
+++ P1HW1_SteinAshley.py
@@ -0,0 +1,18 @@
+# Ashley Stein
+# 3/4/2025
+# P1HW1
+# Write a code that does mathematical equations
+
+print(f"-----Calculating Exponents----")
+base = int(input("Enter base: "))
+exponent = int(input("Enter exponent: "))
+result = base ** exponent
+print(f"{base} raised to the power of {exponent} = {result}")
+
+print(f"-----Addition and Subtraction----")
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+sum_of_first_two = num1 + num2
+final_result = sum_of_first_two - num3
+print(f"{num1} + {num2} - {num3} = {final_result}!")
